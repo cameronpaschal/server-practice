f008eeace64d9201ce3d6c7603e73b162f66d8b0
cameronpaschal <cpaschy@byui.edu>
2025-10-29 20:26:50 -0600

    added handling with mime type

--- http-client.py
+++ http-client.py
@@ -3,15 +3,19 @@ from tqdm import tqdm
 import os
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 import chardet
+import mimetypes
+
+
 
 headers = {"Authorization": "Bearer simpleToken"}
 url = "http://100.79.4.20:5001/upload"
 path = "video.MP4"
+mime_type, _ = mimetypes.guess_type(path)
 
 encoder = MultipartEncoder(
     fields={
         "rel_dir": "images/",
-        "file": ("video.MP4", open(path, "rb"), "video/mp4")
+        "file": ("video.MP4", open(path, "rb"), mime_type or "application/octet-stream")
     }
 )
 
